d90111e30ab8ad93b9d515aa0f0b32b9dc537cad
MEA <mayer_evgenija@mail.ru>
2021-07-08 00:56:41 +0700

    Добавила еще один фрагмент - для текста заметок
    Фрагмент NoteDescriptionFragment переименовала в NoteListFragment - для списка заметок; для текста - NoteDescriptionFragment; удалила какой-то метод в первом фрагменте, т.к. он не понадобился

--- app/src/main/java/keyone/keytwo/homework3/NoteDescriptionFragment.java
+++ app/src/main/java/keyone/keytwo/homework3/NoteDescriptionFragment.java
@@ -2,39 +2,16 @@ package keyone.keytwo.homework3;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 public class NoteDescriptionFragment extends Fragment {
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_note_description, container, false);
-    }
-
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        createTextViewList((LinearLayout) view);
-    }
-
-    private void createTextViewList(LinearLayout linearLayout) {
-        String[] noteName = getResources().getStringArray(R.array.note_name);
-        for (int i = 0; i < noteName.length; i++) {
-            TextView textView = new TextView(getContext());
-            textView.setText(noteName[i]);
-            textView.setTextSize(25);
-            linearLayout.addView(textView);
-        }
+    public NoteDescriptionFragment() {
+        // Required empty public constructor
     }
 
     public static NoteDescriptionFragment newInstance(String param1, String param2) {
@@ -50,5 +27,12 @@ public class NoteDescriptionFragment extends Fragment {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
         }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_note_description, container, false);
     }
 }
---
+++ app/src/main/java/keyone/keytwo/homework3/NoteListFragment.java
@@ -0,0 +1,44 @@
+package keyone.keytwo.homework3;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class NoteListFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_note_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        createTextViewList((LinearLayout) view);
+    }
+
+    private void createTextViewList(LinearLayout linearLayout) {
+        String[] noteName = getResources().getStringArray(R.array.note_name);
+        for (int i = 0; i < noteName.length; i++) {
+            TextView textView = new TextView(getContext());
+            textView.setText(noteName[i]);
+            textView.setTextSize(25);
+            linearLayout.addView(textView);
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+}
