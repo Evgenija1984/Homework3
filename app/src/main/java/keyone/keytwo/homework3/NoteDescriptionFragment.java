2bc5f3c41d0a2f9859155c739dc44c6fdc99974b
MEA <mayer_evgenija@mail.ru>
2021-07-07 17:24:12 +0700

    Создала фрагмент для списка заметок
    Удалила лишние строчки в коде фрагмента; в мекете удалила текст вью

---
+++ app/src/main/java/keyone/keytwo/homework3/NoteDescriptionFragment.java
@@ -0,0 +1,34 @@
+package keyone.keytwo.homework3;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NoteDescriptionFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_note_description, container, false);
+    }
+
+    public static NoteDescriptionFragment newInstance(String param1, String param2) {
+        NoteDescriptionFragment fragment = new NoteDescriptionFragment();
+        Bundle args = new Bundle();
+        //args.putString(ARG_PARAM1, param1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+}
