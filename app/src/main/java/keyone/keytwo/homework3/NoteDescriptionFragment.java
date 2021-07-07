a00b01ce6f85fbedcd5734df328cfdf842529023
MEA <mayer_evgenija@mail.ru>
2021-07-07 17:41:41 +0700

    Добавила метод onViewCreated

--- app/src/main/java/keyone/keytwo/homework3/NoteDescriptionFragment.java
+++ app/src/main/java/keyone/keytwo/homework3/NoteDescriptionFragment.java
@@ -2,11 +2,15 @@ package keyone.keytwo.homework3;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class NoteDescriptionFragment extends Fragment {
 
@@ -15,6 +19,19 @@ public class NoteDescriptionFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_note_description, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        LinearLayout linearLayout = (LinearLayout) view;
+        String[] noteName = getResources().getStringArray(R.array.note_name);
+        for(int i = 0; i < noteName.length; i++){
+            TextView textView = new TextView(getContext());
+            textView.setText(noteName[i]);
+            textView.setTextSize(25);
+            linearLayout.addView(textView);
+        }
     }
 
     public static NoteDescriptionFragment newInstance(String param1, String param2) {
