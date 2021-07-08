7c44413c7f9c2e4775f2d4472b7de3240a82103c
MEA <mayer_evgenija@mail.ru>
2021-07-08 17:51:59 +0700

    добавила метод onActivityCreated

--- app/src/main/java/keyone/keytwo/homework3/NoteListFragment.java
+++ app/src/main/java/keyone/keytwo/homework3/NoteListFragment.java
@@ -1,8 +1,10 @@
 package keyone.keytwo.homework3;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -15,6 +17,16 @@ import android.widget.TextView;
 public class NoteListFragment extends Fragment {
 
     private boolean isLandscape;
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        isLandscape = getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
+        if (isLandscape) {
+            NoteDescriptionFragment noteDescriptionFragment = NoteDescriptionFragment.newInstance(0);
+            requireActivity().getSupportFragmentManager().beginTransaction().replace(R.id.notes_description_land_container, noteDescriptionFragment).commit();
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
