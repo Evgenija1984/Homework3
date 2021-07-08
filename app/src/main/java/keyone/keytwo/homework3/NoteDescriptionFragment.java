3f7ace5709aed7fcc65e5688b9c4bbf0c37e4e59
MEA <mayer_evgenija@mail.ru>
2021-07-08 14:51:45 +0700

    Во фрагменте NoteDescriptionFragment добавила ключ и значение В методе onCreateView что-то там накодила, чтобы получать картинки. В ресурсах в интовом массиве добавила ссылку на картинки, которые довабила в drawable

--- app/src/main/java/keyone/keytwo/homework3/NoteDescriptionFragment.java
+++ app/src/main/java/keyone/keytwo/homework3/NoteDescriptionFragment.java
@@ -1,5 +1,6 @@
 package keyone.keytwo.homework3;
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,17 +8,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+// это типа гербы
 public class NoteDescriptionFragment extends Fragment {
+
+    public static final String KEY_INDEX = "index";
+    private int index;
 
     public NoteDescriptionFragment() {
         // Required empty public constructor
     }
 
-    public static NoteDescriptionFragment newInstance(String param1, String param2) {
+    public static NoteDescriptionFragment newInstance(int index) {
         NoteDescriptionFragment fragment = new NoteDescriptionFragment();
         Bundle args = new Bundle();
-        //args.putString(ARG_PARAM1, param1);
+        //args.putString(KEY_INDEX, index);
+        args.putInt(KEY_INDEX, index);
         fragment.setArguments(args);
         return fragment;
     }
@@ -26,13 +33,17 @@ public class NoteDescriptionFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
+            index = getArguments().getInt(KEY_INDEX);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_note_description, container, false);
+        View view = inflater.inflate(R.layout.fragment_note_description, container, false);
+        ImageView imageView = view.findViewById(R.id.coat_of_arms);
+        TypedArray images = getResources().obtainTypedArray(R.array.numbers);
+        imageView.setImageResource(images.getResourceId(index, -1));
+        return view;
     }
 }
