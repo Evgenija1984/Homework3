3f48327ee0fdb233682bf69210351dc2cd26b1aa
MEA <mayer_evgenija@mail.ru>
2021-07-08 15:03:48 +0700

    Добавила еще активити для портретного отображения NoteDescriptionPortActivity
    В макете добавила контейнер для фрагмента

---
+++ app/src/main/java/keyone/keytwo/homework3/NoteDescriptionPortActivity.java
@@ -0,0 +1,14 @@
+package keyone.keytwo.homework3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NoteDescriptionPortActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_description_port);
+    }
+}
