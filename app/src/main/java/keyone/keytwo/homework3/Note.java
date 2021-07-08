045d9c801c7b637561c11f4a5119044718c23838
MEA <mayer_evgenija@mail.ru>
2021-07-08 23:29:37 +0700

    В классе Note подредактировала код

--- app/src/main/java/keyone/keytwo/homework3/Note.java
+++ app/src/main/java/keyone/keytwo/homework3/Note.java
@@ -1,10 +1,44 @@
 package keyone.keytwo.homework3;
 
+import android.content.Context;
 import android.os.Parcel;
 import android.os.Parcelable;
 
 public class Note implements Parcelable {
+    private int noteIndex;
+    private static final String KEY_NOTE_YEAR = "key_note_year_";
+    private static final String KEY_NOTE_MONTH = "key_note_monthOfYear_";
+    private static final String KEY_NOTE_DAY = "";
+    private static final String KEY_PREF = "note_date";
+
+    public Note(int contentIndex){
+        this.noteIndex = contentIndex;
+    }
+
+    public int getNoteIndex() {
+        return noteIndex;
+    }
+
+    public String getNoteName(Context mContext) {
+        return mContext.getResources().getStringArray(R.array.note_name)[noteIndex];
+    }
+
+    public String getNoteBody(Context mContext) {
+        return mContext.getResources().getStringArray(R.array.note_body)[noteIndex];
+    }
+
     protected Note(Parcel in) {
+        noteIndex = in.readInt();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(getNoteIndex());
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
     }
 
     public static final Creator<Note> CREATOR = new Creator<Note>() {
@@ -19,12 +53,4 @@ public class Note implements Parcelable {
         }
     };
 
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-    }
 }
